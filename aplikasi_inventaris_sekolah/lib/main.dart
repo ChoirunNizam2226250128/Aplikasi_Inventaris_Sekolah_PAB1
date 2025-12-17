701ca98a30e559d3cc87f84b1beee5c3e3f585a9
Choirun Nizam <choirunnizam_2226250128@mhs.mdp.ac.id>
2025-12-18 01:19:32 +0700

    add splash screen

--- aplikasi_inventaris_sekolah/lib/main.dart
+++ aplikasi_inventaris_sekolah/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart'; // ✅ TAMBAH INI
 import 'screens/login_screen.dart';
 
 void main() {
@@ -13,12 +14,14 @@ class InventarisApp extends StatelessWidget {
     return MaterialApp(
       title: 'Inventaris Sekolah',
       debugShowCheckedModeBanner: false,
+
+      home: const SplashScreen(),
+
       theme: ThemeData(
         colorSchemeSeed: const Color.fromARGB(255, 91, 104, 19),
         useMaterial3: true,
         fontFamily: 'Poppins',
       ),
-      home: const LoginScreen(),
     );
   }
 }
---
+++ aplikasi_inventaris_sekolah/lib/screens/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      body: Center(
+        child: Image.asset(
+          'assets/images/inventaris_sekolah.png',
+          width: MediaQuery.of(context).size.width * 0.9,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
